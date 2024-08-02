3fb12c3592c5be2b7a5bc872e8db50aa3f5a548a
Catalyst <Catalyst>
2024-08-02 22:31:06 +0100

    addition of latest stuff.

--- app/components/Home/SideMenu.jsx
+++ app/components/Home/SideMenu.jsx
@@ -1,5 +1,5 @@
-'use client'
-import React, {useRef, useEffect} from "react";
+"use client";
+import React, { useRef, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ProfileBox from "./Home/ProfileBox";
 import { MdClose } from "react-icons/md";
@@ -8,19 +8,20 @@ import { setSideBarOpen } from "@/GlobalRedux/slices/AppSlice";
 function SideMenu() {
   const dispatch = useDispatch();
   const { sideBarOpen } = useSelector((st) => st.app);
-  const sideMenu = useRef(null)
+  const sideMenu = useRef(null);
 
-  useEffect(() => {
-      sideMenu.current.classList.toggle('translate-x-[100%]')
-  }, [sideBarOpen])
+  // useEffect(() => {
+  //     if(sideBarOpen) sideMenu.current.classList.remove('translate-x-[100%]')
+  //     else sideMenu.current.classList.add('translate-x-[100%]')
+  // }, [sideBarOpen]) 
   return (
     <>
       {sideBarOpen && (
         <div className="fixed overlay inset-0 bg-black/50 z-[9990]"></div>
       )}
       <div
-      ref={sideMenu}
-        className={`side-menu fixed z-[9999] top-0 bottom-0 right-0 bg-[#f7f8fd] p-8 translate-x-[100%] duration-300 `}
+        ref={sideMenu}
+        className={`side-menu fixed z-[9999] top-0 bottom-0 right-0 bg-[#f7f8fd] p-8 duration-300 `}
       >
         <MdClose
           className="absolute left-4 top-4"
